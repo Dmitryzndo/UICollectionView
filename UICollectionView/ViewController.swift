593abdda09d2c2c716168939ff0ff35d9a749ddc
dimkasp@mail.ru <dimkasp@mail.ru>
2019-04-20 20:06:07 +0300

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Dmitry Belousov on 20/04/2019.
+//  Copyright © 2019 Dmitry Belousov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
